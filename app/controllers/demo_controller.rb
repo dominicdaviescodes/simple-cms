87e6c30ed3ba8760f64705d4aac29ff846729e6d
dominicdaviescodes <codewithdom@gmail.com>
2020-03-23 21:38:12 +0000

    complete section 3 challenge

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -4,4 +4,23 @@ class DemoController < ApplicationController
   
   def index
   end
+
+  def hello
+  end
+
+  def about
+    #static
+    render('about_us')
+  end
+
+  def contact
+    if ['us', 'ca'].include?(params[:country])
+      @phone = '(800) 555-6789'
+    elsif params[:country] == 'uk'
+      @phone = '020 555-6789'
+    else
+      @phone = '(+1) 555-6789'
+    end
+      render('contact_us')
+  end
 end
